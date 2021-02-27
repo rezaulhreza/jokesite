75f903579c69d093f6cce31e9945e530497447e0
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2021-02-27 14:06:04 +0000

    Edit page content load fixed

--- templates/editJoke.html.php
+++ templates/editJoke.html.php
@@ -1,6 +1,6 @@
 <form action="" method="post">
 <input type="hidden" name="id" value="<?=$joke['id'] ?>" />
     <label for="joketext">Update your joke:</label>
-    <textarea id="joketext" name="joketext" values="<?=$joke['joketext'] ?>" rows="3" cols="40"></textarea>
+    <textarea id="joketext" name="joketext" rows="3" cols="40" required><?=$joke['joketext']?></textarea>
     <input type="submit" value="Update">
 </form>
